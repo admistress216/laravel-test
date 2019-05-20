ae3fefc80c55afe99823d17e4d211b25ac68b1b6
lizengcai <lizengcai_vip@sina.com>
2019-05-20 17:16:55 +0800

    添加登录模板

--- app/Http/Controllers/Admin/EntryController.php
+++ app/Http/Controllers/Admin/EntryController.php
@@ -7,8 +7,18 @@ use App\Http\Controllers\Controller;
 
 class EntryController extends Controller
 {
-    //
+    /**
+     * 登录视图
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function loginForm(){
-        return 'abc';
+        return view('admin/entry/login');
+    }
+
+    /**
+     * 登录认证
+     */
+    public function login() {
+
     }
 }
--- routes/admin/web.php
+++ routes/admin/web.php
@@ -3,5 +3,5 @@
 //  /admin/abc
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
     Route::get('login', 'EntryController@loginForm');
-
+    Route::post('login', 'EntryController@login');
 });
