1d121c3433b975e7e4dcb6199ac00932dbd2ed28
lizengcai <lizengcai_vip@sina.com>
2019-05-21 16:24:22 +0800

    修改登录验证bug

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,8 +16,8 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (!Auth::guard('admin')->attempt()) {
-            return redirect('Admin/login');
+        if (!Auth::guard('admin')->check()) {
+            return redirect('admin/login');
         }
         return $next($request);
     }
